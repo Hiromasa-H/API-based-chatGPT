59ff207361a3f0ca477587908046a10a3414fadf
Hiromasa-H <hhiromasa217@gmail.com>
2023-05-23 14:37:24 +0900

    commented out everything

--- static/script.js
+++ static/script.js
@@ -1,20 +1,20 @@
-const messageForm = document.getElementById('message-form');
-const messageInput = document.getElementById('message-input');
-const messagesContainer = document.getElementById('messages');
+// const messageForm = document.getElementById('message-form');
+// const messageInput = document.getElementById('message-input');
+// const messagesContainer = document.getElementById('messages');
 
-messageForm.addEventListener('submit', async (event) => {
-  event.preventDefault();
+// messageForm.addEventListener('submit', async (event) => {
+//   event.preventDefault();
 
-  const message = messageInput.value;
-  const renderedMessage = await sendMessage(message);
-  messageInput.value = '';
+//   const message = messageInput.value;
+//   const renderedMessage = await sendMessage(message);
+//   messageInput.value = '';
 
-  const messageElement = document.createElement('div');
-  messageElement.innerHTML = renderedMessage;
-  messagesContainer.appendChild(messageElement);
-});
+//   const messageElement = document.createElement('div');
+//   messageElement.innerHTML = renderedMessage;
+//   messagesContainer.appendChild(messageElement);
+// });
 
-async function sendMessage(message) {
-  const response = await fetch(`/message?text=${encodeURIComponent(message)}`);
-  return await response.text();
-}
+// async function sendMessage(message) {
+//   const response = await fetch(`/message?text=${encodeURIComponent(message)}`);
+//   return await response.text();
+// }
